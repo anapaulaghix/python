e54aee68a2edcee732c1bcf7de094c43666d1371
ana paula <anapaulaghis@gmail.com>
2021-06-24 00:16:29 -0300

    Initial commit

---
+++ recebendo_dados.py
@@ -0,0 +1,9 @@
+nome = input("Qual seu nome?")
+print(f"Seja Bem Vindo(a) {nome}")
+
+idade = int(input("Qual sua idade? "))
+print(f"Você tem {idade} anos")
+
+print(f"{nome} nasceu em {2021 - idade}")
+
+# input() -> Todo dado recebido via input é do tipo String

be7bdc34d3719b6217aa2f430bb1216f05f59ba8
ana paula <anapaulaghis@gmail.com>
2021-07-18 17:09:39 -0300

    .

--- dict.py
+++ dict.py
@@ -1,3 +1,17 @@
+"""
+Módulos Collections
+- Counter (tranformar em "dicionário")
+- defaultdict (criar chaves no dict)
+- ordereddict (ordenar valores no dict)
+- namedtuple (criamos um dado com um nome e parâmetro)
+- deque (transforma qualquer dado em lista)
+"""
+from collections import Counter
+lista = [1, 1, 1, 2, 2, 2, 2, 4, 6, 67, 45, 3, 3, 3, 45, 6, 6, 6, 7]
+res = Counter(lista)
+print(res)
+print(Counter('Ana paula'))
+
 # Coleções do tipo chave/valor
 # em outras linguagens são chamados de mapas
 # não são indexados
@@ -58,3 +72,23 @@ produto2 = {'nome:': 'Stardew Valley', 'quantidade:': 1, 'preço:': 25.0}
 carrinho.append(produto1)
 carrinho.append(produto2)
 print(carrinho)
+
+"""
+Sets - conjuntos
+- representados por chaves {}
+- não tem valores duplicados
+"""
+s = {23, 44, 56, 1, 2, 65, 4, 4, 1}
+print(s)
+print(type(s))
+print(len(set(s)))  # não existe repetição
+
+# revisando coleções
+lista = [1, 99, 76, 45, 45, 3, 1]
+print(f'Lista: {lista} com {len(lista)} elementos')
+tupla = 1, 99, 76, 45, 45, 3, 1
+print(f'Tupla: {tupla} com {len(tupla)} elementos')
+dicionario = {}.fromkeys([1, 99, 76, 45, 45, 3, 1], 'dict')
+print(f'Dicionário: {dicionario} com {len(dicionario)} elementos')
+sets = {1, 99, 76, 45, 45, 3, 1}
+print(f'Conjuntos: {sets} com {len(sets)} elementos')
